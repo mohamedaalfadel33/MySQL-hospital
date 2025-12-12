4eee17e580104582f770b9149045071fff9fd03a
mohamedaalfadel33 <mohamed1239455@gmail.com>
2025-12-12 10:36:54 +0200

    page 1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,21 @@
-<script setup></script>
+<script setup>
+import Layout from './components/layouts/Layout.vue';
+import Dashboard from './components/pages/Dashboard.vue';
+import Welcome from './components/pages/Welcome.vue';
+import Workout from './components/pages/Workout.vue';
+
+const selectedDisplay = 1;
+</script>
 
 <template>
-  <div class="flex bg-red-200">
-    <h1>hello</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
-    <h1>hello</h1>
-  </div>
+  <Layout>
+    <!-- PAGE1 -->
+    <Welcome v-if="selectedDisplay === 1" />
+    <!-- PAGE2 -->
+    <Dashboard v-if="selectedDisplay === 2" />
+    <!-- PAGE3 -->
+    <Workout v-if="selectedDisplay === 3" />
+  </Layout>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/layouts/Layout.vue
@@ -0,0 +1,68 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1 class="text-gradient">SMOLGRAM</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <small>Created by</small>
+    <a href="https://github.com/mohamedaalfadel33" target="_blank">
+      <img
+        src="https://avatars.githubusercontent.com/u/108083797?v=4"
+        alt="pfp"
+      />
+      <p>
+        Mohamed</p>
+      <i class="fa-brands fa-github"></i>
+    </a>
+  </footer>
+</template>
+
+<style scoped>
+header,
+footer,
+main {
+  padding: 1rem;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+main {
+  flex: 1;
+}
+
+footer a {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.3rem;
+  padding-right: 0.5rem;
+  background: var(--background-muted);
+  border-radius: 4rem;
+  border: 1px solid transparent;
+  transition-duration: 200ms;
+  text-decoration: none;
+}
+
+footer a img {
+  max-width: 30px;
+  aspect-ratio: 1/1;
+  border-radius: 100%;
+}
+
+footer a:hover {
+  border-color: var(--color-link);
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: center;
+  padding: 2rem o;
+  padding-bottom: 3rem;
+}
+</style>
