c023a306acc6229c131280653668c804975863a2
mohamedaalfadel33 <mohamed1239455@gmail.com>
2025-12-12 08:38:25 +0200

    vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="flex bg-red-200">
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+  </div>
+</template>
+
+<style scoped></style>
